a277de3bfae3cb4d5c0f2fb22eb0aa1c0d09e989
coling01 <colin.greenwood@hmcts.net>
2025-12-04 12:25:47 +0000

    feature: amp-106 further tidy project * Switch to using flyway in int tests * Switch to using example database table and repository to line up with service and controller which were already example * Switch h2 database to TestContainers

--- src/test/java/uk/gov/hmcts/cp/integration/ExampleControllerIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/cp/integration/ExampleControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.cp.NonTracingIntegrationTestSetup;
