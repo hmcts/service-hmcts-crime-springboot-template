3b4be05d53c1838dfe437e10e0adfd60024d3c96
madhusudan.yasa <madhusudan.yasa@hmcts.net>
2025-12-11 11:19:07 +0000

    AMP-135 - addressed review comments.

--- docker/Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM eclipse-temurin:21
-WORKDIR /opt/app
-COPY build/libs/*.jar /opt/app/app.jar
-CMD ["java", "-jar", "/opt/app/app.jar"]
--- src/test/java/uk/gov/hmcts/cp/repositories/ExampleRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/cp/repositories/ExampleRepositoryTest.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.cp.entities.ExampleEntity;
 import uk.gov.hmcts.cp.integration.IntegrationTestBase;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ExampleRepositoryTest extends IntegrationTestBase {
 
@@ -25,7 +24,7 @@ class ExampleRepositoryTest extends IntegrationTestBase {
     void save_and_query_should_return_entity_by_case_id() {
         ExampleEntity savedEntity = exampleRepository.save(ExampleEntity.builder().exampleText("Some info").build());
         ExampleEntity retrievedEntity = exampleRepository.getReferenceById(savedEntity.getId());
-        assertNotNull(retrievedEntity.getId());
+        assertThat(retrievedEntity.getId()).isNotNull();
         assertThat(retrievedEntity.getExampleText()).isEqualTo("Some info");
     }
 }
