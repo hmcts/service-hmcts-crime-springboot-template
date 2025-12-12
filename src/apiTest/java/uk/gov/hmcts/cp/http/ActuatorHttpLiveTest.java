779911d3dcb4500cf51407ca8c676822b8f5839c
madhusudan.yasa <madhusudan.yasa@hmcts.net>
2025-12-12 17:41:24 +0000

    AMP-135 - addressed merge conflicts.

--- src/apiTest/java/uk/gov/hmcts/cp/http/ActuatorHttpLiveTest.java
+++ src/apiTest/java/uk/gov/hmcts/cp/http/ActuatorHttpLiveTest.java
@@ -2,7 +2,12 @@ package uk.gov.hmcts.cp.http;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
