c5698a88ab7c60ff7f78e3a7de852a7887099117
coling01 <colin.greenwood@hmcts.net>
2026-02-10 12:11:30 +0000

    chore: temp add the cert keystore stuff

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,12 @@ WORKDIR /app
 # copy startup script and app jar file
 COPY docker/* /app/
 COPY build/libs/*.jar /app/
-COPY ../lib/applicationinsights.json /app/
+COPY lib/applicationinsights.json /app/
+
+# Not sure this does anything useful we can drop once we sort certificates
+RUN test -n "$JAVA_HOME" \
+ && test -f "$JAVA_HOME/lib/security/cacerts" \
+ && chmod 777 "$JAVA_HOME/lib/security/cacerts" \
 
 USER app
 ENTRYPOINT ["/bin/sh","./startup.sh"]
--- docker/startup.sh
+++ docker/startup.sh
@@ -8,6 +8,34 @@ logmsg() {
     echo "$SCRIPTNAME : $1"
 }
 
+logmsg "running and loading certificates ..."
+if [ -z "$JAVA_HOME" ]; then
+    export JAVA_HOME="/usr/local/openjdk-21"
+fi
+export KEYSTORE="$JAVA_HOME/lib/security/cacerts"
+if [ -z "$CERTS_DIR" ]; then
+    logmsg "Warning - expects \$CERTS_DIR to be set. i.e. export CERTS_DIR="/etc/certs
+    logmsg "Defaulting to /etc/certs"
+    export CERTS_DIR="/etc/certs"
+fi
+
+if [ ! -f "$KEYSTORE" ]; then
+    logmsg "Error - expects keystore $KEYSTORE to already exist"
+    exit 1
+fi
+
+export count=1
+logmsg "Loading certificates from $CERTS_DIR into keystore $KEYSTORE"
+for FILE in $(ls $CERTS_DIR)
+do
+    alias="mojcert$count"
+    logmsg "Adding $CERTS_DIR/$FILE to keystore with alias $alias"
+    keytool -importcert -file $CERTS_DIR/$FILE -keystore $KEYSTORE -storepass changeit -alias $alias -noprompt
+    count=$((count+1))
+done
+
+keytool -list -keystore $KEYSTORE -storepass changeit | grep "Your keystore contains"
+
 export LOCALJARFILE=$(ls ./build/libs/*.jar 2>/dev/null | grep -v 'plain' | head -n1)
 export DOCKERJARFILE=$(ls /app/*.jar 2>/dev/null | grep -v 'plain' | head -n1)
 if [ -f "$DOCKERJARFILE" ]; then
